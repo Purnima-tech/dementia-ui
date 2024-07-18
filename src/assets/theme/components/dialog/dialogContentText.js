ed9ef58ce350cebaa6723aad4484287d2e6bd119
ketanrathod114 <ketanrathod114@gmail.com>
2024-07-18 17:19:59 +0530

    Initial Commit

---
+++ src/assets/theme/components/dialog/dialogContentText.js
@@ -0,0 +1,22 @@
+/**/
+
+// Dementia care React base styles
+import typography from "assets/theme/base/typography";
+import colors from "assets/theme/base/colors";
+
+// Dementia care React helper functions
+// import pxToRem from "assets/theme/functions/pxToRem";
+
+const { size } = typography;
+const { text } = colors;
+
+const dialogContentText = {
+  styleOverrides: {
+    root: {
+      fontSize: size.md,
+      color: text.main,
+    },
+  },
+};
+
+export default dialogContentText;
---
+++ src/assets/theme/components/stepper/step.js
@@ -0,0 +1,14 @@
+/**/
+
+// Dementia care React helper functions
+import pxToRem from "assets/theme/functions/pxToRem";
+
+const step = {
+  styleOverrides: {
+    root: {
+      padding: `0 ${pxToRem(6)}`,
+    },
+  },
+};
+
+export default step;
